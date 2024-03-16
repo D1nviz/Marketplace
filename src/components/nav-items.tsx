e80844343c225dfba908efe1eb8c3ede75b9b074
Dinviz <eugene.hetsyanyn@gmail.com>
2024-03-16 18:05:01 +0200

    feature/added navbar, cart

--- src/components/nav-items.tsx
+++ src/components/nav-items.tsx
@@ -1,12 +1,44 @@
 "use client";
 
-import { useState } from "react";
+import { productCategories } from "@/config";
+import { useEffect, useRef, useState } from "react";
+import NavItem from "@/components/nav-item";
+import { useOnClickOutside } from "@/hooks/use-on-click-outside";
 
 const NavItems = () => {
   const [activeIndex, setActiveIndex] = useState<null | number>(null);
-  return (
-    <div className="flex gap-4 h-full">
 
+  const navRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    const handler = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setActiveIndex(null);
+      }
+    };
+    document.addEventListener("keydown", handler);
+    return () => document.removeEventListener("keydown", handler);
+  }, []);
+
+  useOnClickOutside(navRef, () => setActiveIndex(null));
+
+  return (
+    <div ref={navRef} className="flex gap-4 h-full">
+      {productCategories.map((category, index) => {
+        const handleOpen = () =>
+          activeIndex === index ? setActiveIndex(null) : setActiveIndex(index);
+
+        const isOpen = activeIndex === index;
+        return (
+          <NavItem
+            key={category.value}
+            category={category}
+            handleOpen={handleOpen}
+            isOpen={isOpen}
+            isAnyOpen={activeIndex !== null}
+          />
+        );
+      })}
     </div>
   );
 };
--- src/config/index.ts
+++ src/config/index.ts
@@ -22,8 +22,30 @@ export const perks = [
 ];
 
 export const productCategories = [
-  {label: "UI Kits", value: "ui-kits" as const, featured: [
-    {name: "Editor picks", href: "#", imageSrc: "/nav/ui-kits/mixed.jpg"}
-  ]} ,
-  {label: "Illustrations", value: "illustrations"} as const,
-]
+  {
+    label: "UI Kits",
+    value: "ui-kits" as const,
+    featured: [
+      { name: "Editor picks", href: "#", imageSrc: "/nav/ui-kits/mixed.jpg" },
+      { name: "New Arrivals", href: "#", imageSrc: "/nav/ui-kits/blue.jpg" },
+      { name: "Bestsellers", href: "#", imageSrc: "/nav/ui-kits/purple.jpg" },
+    ],
+  },
+  {
+    label: "Icons",
+    value: "icons" as const,
+    featured: [
+      {
+        name: "Favorite Icon Picks ",
+        href: "#",
+        imageSrc: "/nav/icons/picks.jpg",
+      },
+      { name: "New Arrivals", href: "#", imageSrc: "/nav/icons/new.jpg" },
+      {
+        name: "Bestselling Icons",
+        href: "#",
+        imageSrc: "/nav/icons/bestsellers.jpg",
+      },
+    ],
+  },
+];
