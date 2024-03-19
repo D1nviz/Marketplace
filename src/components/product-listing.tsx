9e7ea701c0e1b94fe8be580d0e58343fab152ba2
Dinviz <eugene.hetsyanyn@gmail.com>
2024-03-19 20:57:12 +0200

    feature/added airBnB-slider

--- src/components/product-listing.tsx
+++ src/components/product-listing.tsx
@@ -5,6 +5,7 @@ import { Skeleton } from "./ui/skeleton";
 import Link from "next/link";
 import { cn, formatPrice } from "@/lib/utils";
 import { productCategories } from "@/config";
+import ImageSlider from "./image-slider";
 
 type ProductListingProps = {
   product: Product | null;
@@ -28,6 +29,10 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
     ({ value }) => value === product.category
   )?.label;
 
+  const validUrls = product.images
+    .map(({ image }) => (typeof image === "string" ? image : image.url))
+    .filter(Boolean) as string[];
+
   if (isVisible && product) {
     return (
       <Link
@@ -37,12 +42,12 @@ const ProductListing = ({ product, index }: ProductListingProps) => {
         href={`/product/${product.id}`}
       >
         <div className="flex flex-col w-full">
+          <ImageSlider urls={validUrls} />
           <h3 className="mt-4 font-medium text-sm text-gray-700">
             {product.name}
           </h3>
           <p className="mt-1 text-sm text-gray-500"> {label}</p>
           <p className="mt-1 font-medium text-sm text-gray-900">
-            {" "}
             {formatPrice(product.price)}
           </p>
         </div>
